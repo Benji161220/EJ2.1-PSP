f4e476ab436d3392f272c7ee8f9c7bf66e54bcee
Benji161220 <139280838+Benji161220@users.noreply.github.com>
2025-11-09 18:42:23 +0100

    Primer Ejercicio hecho

--- src/EJ01Runnable.java
+++ src/EJ01Runnable.java
@@ -1,21 +1,30 @@
-public class EJ01Runnable{
-    public static void main(String[] args){
+public class Ej01Runnable {
+    public static void main(String[] args) {
         int num_hilos = 8;
-        Thread[] hijos = new Thread[num_hilos];
-        for (int i = 0; i < num_hilos; i++) {
-            hijos[i] = new Thread(new MITarea());
+        Thread[] hilos = new Thread[num_hilos];
+        for (int i=0;i<num_hilos;i++){
+            hilos[i] = new Thread(new MiTareaRunnable());
+            hilos[i].start();
+        }
+        for (int i=0;i<num_hilos;i++){
+            try {
+                hilos[i].join();
+                System.out.println("El programa ha finalizado correctamente");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
-class MITarea implements Runnable{
+class MiTareaRunnable implements Runnable{
     @Override
     public void run() {
-        for(int i = 1; i<=5; i++){
-            System.out.println("Soy el hilo "+ Thread.currentThread().threadId());
+        for (int i=1;i<6;i++){
+            System.out.println("Soy el hilo: "+Thread.currentThread().getId()+", mi cont: "+i);
             try {
                 Thread.sleep(1000);
-            } catch (InterruptedException e){
-                throw  new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
     }
